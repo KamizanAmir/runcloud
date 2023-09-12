259ef20db54ae4021f4fb7885e50bdff28b09671
KamizanAmir <kamizan980505@gmail.com>
2023-09-13 05:38:28 +0800

    create workspace model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Workspace') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Add a workspace by press the "+" button below') }}
                 </div>
             </div>
         </div>
