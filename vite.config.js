e9bb859bc0703ae5f6cded0b9f17600834ee63f1
KamizanAmir <kamizan980505@gmail.com>
2023-09-13 03:46:47 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+          output: {
+            // Make sure to set the proper public path
+            // if your app is not hosted at the root of the domain
+            publicPath: '/build/',
+          },
+        },
+      },
+    plugins: [
+        LaravelVitePlugin(),
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+});
